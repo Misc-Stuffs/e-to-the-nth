6e9257c0cd7d42eec4541a1985db9b8ffee6180f
Benjamin Worthington <benworthin@icloud.com>
2020-12-17 16:51:15 -0600

    Implemented basic solution with limit checking loop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+const float E = exp(1.0);
+const int LIMIT = 20;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int numberOfDecimals = 0;
+
+    cout << "Enter how many decimal places of e to generate... (up to 20) > ";
+
+    while (cin >> numberOfDecimals) {
+
+        if (numberOfDecimals <= LIMIT) {
+
+            cout << endl;
+
+            numberOfDecimals++;
+
+            cout << "e to " << numberOfDecimals - 1 << " decimal places: " << setprecision(numberOfDecimals) << E << endl;
+
+            return 0;
+
+        } else {
+
+            cout << "The limit is " << LIMIT << " decimal places. Try again. " << endl;
+            cout << "> ";
+
+        }
+
+    }
+
     return 0;
+
 }
